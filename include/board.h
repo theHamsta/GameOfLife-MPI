eef41347c61b5027882e66767befd7792225222c
theHamsta <stephan.lauf@yahoo.de>
2015-10-26 20:59:24 -0200

    - save current state

--- include/board.h
+++ include/board.h
@@ -3,10 +3,29 @@
 
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define BACTERIA_PER_FIELD 4
 
-typedef uint32_t field_t;
+
+
+
+#define FIELD_ABOVE_MASK (0x7)
+#define FIELD_BELOW_MASK (0x7 << 6)
+#define FIELD_LEFT_AND_RIGHT_MASK (0x5 << 12)
+
+#define FIELD_NEIGHBOUR_MASK ( FIELD_ABOVE_MASK || FIELD_BELOW_MASK || FIELD_LEFT_AND_RIGHT_MASK );
+
+typedef struct field_s 
+{
+	unsigned int above:6;
+	unsigned int below:6;
+	unsigned int right:1;
+	unsigned int oldCells:4;
+	unsigned int left:1;
+	unsigned int newCells:4;
+} field_t;
+
 
 
 typedef struct board_s
@@ -26,6 +45,9 @@ void board_print(board_t* board);
 
 void board_destroy(board_t* board);
 
-void board_step_local(board_t* newBoard, board_t* oldBoard);
+void board_step(board_t* board);
+
+bool board_value_at( board_t* board, unsigned int x, unsigned int y );
+
 
 #endif // BOARD_H
--- src/board.c
+++ src/board.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 #include <string.h>
 
+
+// from: http://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer
+uint32_t popcount(uint32_t i)
+{
+     i = i - ((i >> 1) & 0x55555555);
+     i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+     return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+}
+
+
 board_t* board_create(unsigned int width, unsigned int height)
 {
 	board_t* board = malloc(sizeof(board_t));
@@ -41,7 +51,71 @@ void board_reset(board_t* board)
 
 void board_print(board_t* board)
 {
+	for ( unsigned int y = 0; y < board->height; y++ ) {
+		for ( unsigned int x = 0; x < board->width; x++ ) {
+			printf("%c", board_value_at(board, x, y) ? 'X' : "_");
+		}
+		printf("\n");
+	}
+}
 
+bool board_value_at(board_t* board, unsigned int x, unsigned int y)
+{
+	field_t field = board->data[ (y * board->width + x) / BACTERIA_PER_FIELD ];
+	return (field.oldCells >> (BACTERIA_PER_FIELD - 1 - (y % BACTERIA_PER_FIELD)) & 1;
+}
+
+
+void field_update( field_t* field ) {
+	for ( int i = 0; i < BACTERIA_PER_FIELD; i++ ) {
+		
+		uint32_t neighbours = (*field >> i) & FIELD_NEIGHBOUR_MASK;
+		uint32_t numNeighbours = popcount(neighbours);
+		
+		bool bWasAlive = (field->oldCells >> i) & 1;
+		
+		bool bIsAlive = false;
+		if( numNeighbours == 3 ) {
+			bIsAlive = true;
+		} else if ( bWasAlive && numNeighbours == 2 ) {
+			bIsAlive = true;
+		}
+		*field.newCells = bIsAlive << i;
+	}
+}
+
+
+void board_step( board_t* board )
+{
+	field_t* above = 0;
+	field_t* current = board->data;
+	field_t* below = board->data + board->width / BACTERIA_PER_FIELD;
+	
+	assert( board->height > 1 && "Board to small to process");
+	
+	for ( unsigned int y = 0; y < board->height; y++ ) {
+		for( unsigned int x = 0; x < board->width / BACTERIA_PER_FIELD; x++ ) {
+			
+			field_update(current);
+			current++;
+		}
+
+	}
+	for ( unsigned int y = 0; y < board->height; y++ ) {
+		for( unsigned int x = 0; x < board->width / BACTERIA_PER_FIELD; x++ ) {
+			
+			field_update(current);
+			
+			
+			
+			above++;
+			current++;
+			below++;
+		}
+		if ( y == 0 ) {
+			above = board->data;
+		} 
+	}
 }
 
 
